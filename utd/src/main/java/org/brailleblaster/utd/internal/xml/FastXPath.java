769a98eb095b04f4167de48fd7be2ab09e5304c8
Michael Whapples <mwhapples@aph.org>
2025-09-23 10:51:22 +0100

    Refactoring.

--- utd/src/main/java/org/brailleblaster/utd/internal/xml/FastXPath.java
+++ utd/src/main/java/org/brailleblaster/utd/internal/xml/FastXPath.java
@@ -19,10 +19,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
-import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
@@ -178,90 +175,6 @@ public class FastXPath {
 				return next;
 			}
 		};
-	}
-
-	/**
-	 * Useful utils that java didn't think should be in the JDK
-	 *
-	 * @param <T>
-	 */
-	public interface StreamableIterable<T> extends Iterable<T> {
-		default Stream<T> stream() {
-			return StreamSupport.stream(
-					spliterator(),
-					false
-			);
-		}
-
-		default List<T> list() {
-			return Lists.newArrayList(this);
-		}
-	}
-
-	public static class NodeIterator implements Iterator<Node> {
-		private final Node startNode;
-		private final boolean stayInsideStartNode;
-		private final boolean forward;
-		private Node nextNode;
-
-		public NodeIterator(Node startNode, boolean stayInsideStartNode, boolean forward) {
-			this.startNode = startNode;
-			this.nextNode = startNode;
-			this.stayInsideStartNode = stayInsideStartNode;
-			this.forward = forward;
-		}
-
-		@Override
-		public boolean hasNext() {
-			return nextNode != null;
-		}
-
-		@Override
-		public Node next() {
-			Node curNode = nextNode;
-			doAdvance();
-			return curNode;
-		}
-
-		public void doAdvance() {
-			if (nextNode.getChildCount() != 0) {
-				nextNode = nextNode.getChild(forward ? 0 : nextNode.getChildCount() - 1);
-			} else {
-				nextNode = itrNextNode(
-						nextNode,
-						stayInsideStartNode ? startNode : null,
-						forward
-				);
-			}
-		}
-
-		/**
-		 * Safe following node impl that stops once outside of the given start node
-		 *
-		 * @param stopNode Parent we are not escaping from
-		 * @param inputCurNode Assumed to be some (maybe nested) child of startNode
-		 */
-		public static Node itrNextNode(Node inputCurNode, Node stopNode, boolean forward) {
-			Node curNode = inputCurNode;
-			//TODO: This will break if inputCurNode is not descendant from stopNode
-			while (stopNode == null || stopNode != curNode) {
-				ParentNode parent = curNode.getParent();
-				if (parent == null) {
-					//No more nodes available
-					return null;
-				}
-				int index = parent.indexOf(curNode);
-				if (forward && index != parent.getChildCount() - 1) {
-					return parent.getChild(index + 1);
-				} else if (!forward && index > 0) {
-					return parent.getChild(index - 1);
-				}
-				//Last entry in parent, get parents sibling
-				curNode = parent;
-			}
-			//Finished getting all childrens parents
-			return null;
-		}
 	}
 
 }
