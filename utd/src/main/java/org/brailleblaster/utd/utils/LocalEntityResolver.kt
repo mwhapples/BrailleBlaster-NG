84ef8b5c88a601531a70746420ceae634913a61f
Michael Whapples <software@mdw.onl>
2025-10-09 10:02:32 +0100

    Fix some warnings.

--- utd/src/main/java/org/brailleblaster/utd/utils/LocalEntityResolver.kt
+++ utd/src/main/java/org/brailleblaster/utd/utils/LocalEntityResolver.kt
@@ -75,7 +75,7 @@ class LocalEntityResolver : EntityResolver2 {
         try {
             val systemIdUri = URI(systemId)
             absoluteSystemId = systemIdUri.isAbsolute
-        } catch (e: URISyntaxException) {
+        } catch (_: URISyntaxException) {
             absoluteSystemId = false
         }
 
@@ -107,7 +107,7 @@ class LocalEntityResolver : EntityResolver2 {
                 ) {
                     return InputSource(sourceURI)
                 }
-            } catch (e: URISyntaxException) {
+            } catch (_: URISyntaxException) {
                 // Do nothing, we try the fallbacks below.
             }
         }
@@ -135,7 +135,7 @@ class LocalEntityResolver : EntityResolver2 {
                         BOMInputStream.builder().setInputStream(Files.newInputStream(Paths.get(sourcePathUri))).get()
                     )
                 }
-            } catch (e: URISyntaxException) {
+            } catch (_: URISyntaxException) {
                 // No problem, fallback.
                 log.error("URI of entity is invalid: {}", baseUri)
             } catch (e: IOException) {
