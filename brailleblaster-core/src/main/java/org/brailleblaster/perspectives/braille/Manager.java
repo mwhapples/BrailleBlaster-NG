5ce0eee94b249060539d54e3129dba7306cb58c1
Michael Whapples <software@mdw.onl>
2026-02-08 12:34:28 +0000

    Fix a warning.

--- brailleblaster-core/src/main/java/org/brailleblaster/perspectives/braille/Manager.java
+++ brailleblaster-core/src/main/java/org/brailleblaster/perspectives/braille/Manager.java
@@ -94,7 +94,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
