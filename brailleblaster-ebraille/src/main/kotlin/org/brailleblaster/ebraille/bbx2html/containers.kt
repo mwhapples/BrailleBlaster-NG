44ac02ca11a7c6b1776c3e72717478b742b1003a
Michael Whapples <mwhapples@aph.org>
2026-03-12 08:56:26 +0000

    More specific list handling.

--- brailleblaster-ebraille/src/main/kotlin/org/brailleblaster/ebraille/bbx2html/containers.kt
+++ brailleblaster-ebraille/src/main/kotlin/org/brailleblaster/ebraille/bbx2html/containers.kt
@@ -17,6 +17,7 @@ package org.brailleblaster.ebraille.bbx2html
 
 import nu.xom.Element
 import org.brailleblaster.bbx.BBX
+import org.brailleblaster.utils.xml.BB_NS
 import org.jsoup.nodes.Node
 
 internal fun Element.processContainer(): Collection<Node> = when(BBX.CONTAINER.getSubType(this)) {
@@ -33,6 +34,13 @@ private fun Element.processBox(): org.jsoup.nodes.Element {
     return org.jsoup.nodes.Element("div").attr("type", boxSymbol).appendChildren(processChildren())
 }
 
-private fun Element.processList(): org.jsoup.nodes.Element {
-    return org.jsoup.nodes.Element("ul").appendChildren(processChildren())
-}
+private data class ListItem(val element: Element, val level: Int)
+
+private fun processListItems(items: List<ListItem>) = org.jsoup.nodes.Element("ul").appendChildren(items.flatMap { it.element.processBlock() })
+
+private fun Element.processList(): org.jsoup.nodes.Element =
+    processListItems(childElements.filter { BBX.BLOCK.LIST_ITEM.isA(it) }.map {
+        val level = it.getAttributeValue("itemLevel", BB_NS)?.toIntOrNull() ?: 0
+        println("Processing item at level $level")
+        ListItem(it, level)
+    })
